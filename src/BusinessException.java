300973c93fa0d05539fd9ed8a559179411805fd2
Guilherme Vieira <guilhermevfelix06@gmail.com>
2024-07-12 16:25:57 -0300

    Implemented custom exception for banking operations

---
+++ src/BusinessException.java
@@ -0,0 +1,7 @@
+public class BusinessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String e) {
+        super(e);
+    }
+}
